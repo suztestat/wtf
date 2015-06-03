0bdb7d97c69961bc11afff0c43a60bbd13a7b373
eHealth <eHealth@eHealth-THINK.Speedport_W_724V_01011603_00_005>
2015-06-03 22:00:22 +0200

    initial checkin

---
+++ PowahCube/src/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+	
+	public static void main(String[] args){
+		
+		//1. PowahCube
+		//1.1 IU = 16 bit = char ; 512 IU Blöcke 
+		//Ausgangsmenge der möglichen Zeichen = A = [n] ; n = 65535 -> char Unicode Tabelle
+		String plaintxt = "? 1 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
+		String key = "aaaaaaaaaaaaaakkaaaabcd34dkkk=?";
+		Permutation.removeRedundantElements(key);
+		//~k = h(k) so hat die i-te V-Slice Permutation PIik die Form PIik= (ci(~k); N).
+		//h(k) = redundate Elemente entfernen
+		//2. Permutation
+		//2.1 V-Slice Permutation
+		//3. Preprocessing
+		//4. Fuzzying
+		//5. Feistel-Netz
+		//6. Entschlüsselung
+	}
+}
---
+++ PowahCube/src/Printer.java
@@ -0,0 +1,8 @@
+
+public class Printer {
+	
+	public static void printOperation(String operation, Object input, Object output){
+		System.out.println("Operation: " + operation + "\nInput: " + input +
+				"\nOutput: " + output);
+	}
+}
