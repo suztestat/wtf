2c4be9b08560d97d1d2dac7b7a3472a7d670e321
Lars <Lars@Lars-PC.fritz.box>
2015-06-03 16:49:07 +0200

    kommentar hinzugefügt

--- Testat_Powahcube/src/Preprocessing.java
+++ Testat_Powahcube/src/Preprocessing.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 
 public class Preprocessing {
 
+	//kommentar!
 	private String input;
 	private final char paddingSymbol='~';
 	private final char terminatorSymbol='#';
